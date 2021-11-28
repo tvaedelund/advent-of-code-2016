a6dc9c4704bb6ffc0fd198887b7fc3f436259bbe
Tomas Vaedelund <tvaedelund@gmail.com>
2021-11-28 18:24:48 +0100

    day 9 improvements but still only part 1

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System.Diagnostics;
 
 Console.WriteLine("AoC 2016 Day08");
-var sw = Stopwatch.StartNew();
-
 var input = File.ReadAllText("input.txt").Trim();
+
 // input = "ADVENT";
 // input = "A(1x5)BC";
 // input = "(3x3)XYZ";
@@ -11,27 +10,60 @@ var input = File.ReadAllText("input.txt").Trim();
 // input = "(6x1)(1x3)A";
 // input = "X(8x2)(3x3)ABCY";
 
-var decomp = string.Empty;
-for (int i = 0; i < input.Length; i++)
+// input = "X(8x2)(3x3)ABCY";
+// input = "(27x12)(20x12)(13x14)(7x10)(1x12)A";
+// input = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN";
+
+var sw = Stopwatch.StartNew();
+
+Console.WriteLine($"Part1: {Solve(input)}");
+Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
+
+Console.WriteLine();
+
+sw.Restart();
+Console.WriteLine($"Part2: {Solve(input, true)}");
+Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
+
+
+int Solve(string data, bool isV2 = false)
 {
-    if (input[i] != '(')
+    var result = 0;
+
+    for (int i = 0; i < input.Length; i++)
     {
-         decomp += input[i];
-         continue;
+        if (input[i] != '(')
+        {
+            result += 1;
+            continue;
+        }
+
+        var seq = (isV2) ? decompV2(data.Substring(i)) : decompV1(data.Substring(i));
+
+        result += seq.len;
+
+        i += seq.pos;
     }
 
-    var pos = input.IndexOf(')', i);
-    var marker = input.Substring(i + 1, pos - i - 1).Split('x').Select(int.Parse).ToArray();
-
-    for (int j = 0; j < marker[1]; j++)
-    {
-        decomp += input.Substring(pos + 1, marker[0]);
-    }
-
-    i = pos + marker[0];
+    return result;
 }
 
-var result = decomp.Length;
+(int len, int pos) decompV1(string seq)
+{
+    var pos = seq.IndexOf(')');
+    var marker = seq.Substring(1, pos - 1).Split('x').Select(int.Parse).ToArray();
 
-Console.WriteLine($"Result: {result}");
-Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
+    var length = marker[0] * marker[1];
+
+    return (length, pos + marker[0]);
+}
+
+(int len, int pos) decompV2(string seq)
+{
+    var pos = seq.IndexOf(')');
+    var marker = seq.Substring(1, pos - 1).Split('x').Select(int.Parse).ToArray();
+
+    var length = marker[0] * marker[1];
+
+    return (length, pos + marker[0]);
+}
